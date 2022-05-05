9485de8e7333f3a4cc9be6ee9b46664591157154
Jieun Han <hanjieun9874@gmail.com>
2022-05-05 17:52:48 +0900

    password change function

--- app/src/main/java/fastcampus/aop_part2_chapteer03/MainActivity.kt
+++ app/src/main/java/fastcampus/aop_part2_chapteer03/MainActivity.kt
@@ -1,11 +1,14 @@
 package fastcampus.aop_part2_chapteer03
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
     private val numberPicker1: NumberPicker by lazy {
@@ -32,6 +35,7 @@ class MainActivity : AppCompatActivity() {
     private val changePasswordButton: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.changePasswordButton)
     }
+    private var changePasswordMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,22 +46,52 @@ class MainActivity : AppCompatActivity() {
         numberPicker3
 
         openButton.setOnClickListener {
+            if (changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
             val passwordFromUser =
                 "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
             if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
                 //startActivity()
             } else {
-                AlertDialog.Builder(this)
-                    .setTitle("실패")
-                    .setMessage("비밀번호가 잘못되었습니다.")
-                    .setPositiveButton("확인") { _, _ ->
+                showErrorAlertDialog()
+            }
+        }
 
-                    }
-                    .create()
-                    .show()
+        changePasswordButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            if (changePasswordMode) {
+                passwordPreferences.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+            } else {
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                } else {
+                    showErrorAlertDialog()
+                }
             }
 
         }
+
+    }
+
+    private fun showErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호가 잘못되었습니다.")
+            .setPositiveButton("확인") { _, _ ->
+
+            }
+            .create()
+            .show()
     }
 }
