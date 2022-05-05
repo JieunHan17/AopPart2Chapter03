e782a8ae19f04dcc6050d41c8246319814ed00d4
Jieun Han <hanjieun9874@gmail.com>
2022-05-05 17:07:35 +0900

    password comparison function

--- app/src/main/java/fastcampus/aop_part2_chapteer03/MainActivity.kt
+++ app/src/main/java/fastcampus/aop_part2_chapteer03/MainActivity.kt
@@ -1,11 +1,63 @@
 package fastcampus.aop_part2_chapteer03
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                //startActivity()
+            } else {
+                AlertDialog.Builder(this)
+                    .setTitle("실패")
+                    .setMessage("비밀번호가 잘못되었습니다.")
+                    .setPositiveButton("확인") { _, _ ->
+
+                    }
+                    .create()
+                    .show()
+            }
+
+        }
     }
 }
